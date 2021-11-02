0ee06423b015fe9d88030f9e8903df7e20729886
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-11-02 14:53:41 +0100

    Jelszó ellenőrzés

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function katt() {
 
 }
 
-function megvaltozott(e) {
+function nevMegvaltozott(e) {
     let hossz = document.getElementById("nevMezo").value.length;
     if (hossz > 20) {
         document.getElementById("nevHossz").style.color = "red";
@@ -12,9 +12,34 @@ function megvaltozott(e) {
     document.getElementById("nevHossz").innerHTML = hossz + "/20";
 }
 
+function jelszoMegvaltozott(e) {
+    let hossz = document.getElementById("jelszoMezo").value.length;
+    if (hossz < 8) {
+        document.getElementById("jelszoHossz").style.color = "red";
+    } else {
+        document.getElementById("jelszoHossz").style.color = "black";
+    }
+    document.getElementById("jelszoHossz").innerHTML = hossz + "/8";
+    //jelszoEllenorzes();
+}
+
+function jelszoEllenorzes(e) {
+    let jelszo = document.getElementById("jelszoMezo").value;
+    let jelszoIsmet = document.getElementById("jelszoMegintMezo").value;
+
+    if (jelszo != jelszoIsmet) {
+        document.getElementById("jelszoEllenorzes").innerHTML = "A jelszavak nem egyeznek meg!";
+    } else {
+        document.getElementById("jelszoEllenorzes").innerHTML = "";
+    }
+}
+
 function init() {
     document.getElementById("gomb").addEventListener("click", katt);
-    document.getElementById("nevMezo").addEventListener("input", megvaltozott);
+
+    document.getElementById("nevMezo").addEventListener("input", nevMegvaltozott);
+    document.getElementById("jelszoMezo").addEventListener("input", jelszoMegvaltozott);
+    document.getElementById("jelszoMegintMezo").addEventListener("change", jelszoEllenorzes);
 }
 
 document.addEventListener("DOMContentLoaded", init);
